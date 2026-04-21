7b5a7683a768f3654e32cb887f2393af06b1606a
Watson Game Studios <watsongamestudios@gmail.com>
2026-04-21 23:25:53 +0200

    Added Mobile Support to Blog Page

--- script.js
+++ script.js
@@ -53,3 +53,13 @@ fetch("blogs.json")
 
     document.getElementById("app").appendChild(clone);
   });
+
+const toggle = document.getElementById("menu-toggle");
+const menu = document.querySelector("nav ul");
+
+if (toggle && menu) {
+  toggle.addEventListener("click", () => {
+    menu.classList.toggle("active");
+    toggle.classList.toggle("active");
+  });
+}
